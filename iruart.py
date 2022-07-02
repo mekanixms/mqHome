60ead9f737a556bbba5e06e3c12f32a534e5da44
cezar.lucan@gmail.com <cezar.lucan@gmail.com>
2022-07-02 19:13:42 +0300

    0.6.3 iuart multi dictionary and mcu2 iuart interface

--- iruart.py
+++ iruart.py
@@ -2,20 +2,63 @@ from peripheral import peripheral, TrueValues
 from machine import Pin, UART
 from time import sleep_ms
 import _thread
-from ubinascii import hexlify
+from ubinascii import hexlify, unhexlify
+from jsu import file_exists
+import json
+from os import listdir
 
 _thread.stack_size(4096*2)
 
 
 @peripheral._trigger
-def sendSignal(s, key):
-    return s.send(key)
+def sendSignal(s, key, dictionary="default"):
+    return s.send(key, dictionary)
+
+
+@peripheral._trigger
+def learnSignal(s, key, dictionary="default"):
+    return s.send(key, dictionary)
+
+
+def filesListAllType(dir="/", typeFilter="ir"):
+    filteredFiles = []
+    allFiles = listdir(dir)
+
+    for f in allFiles:
+        if len(f.split(".")) > 1:
+            if f.split(".")[1] == typeFilter:
+                filteredFiles.append(f.split(".")[0])
+
+    return filteredFiles
+
+
+def exportJsonDictionary(cfn, obj):
+    configFile = open(cfn, "w")
+    configFile.write(json.dumps(obj))
+    configFile.close()
+
+
+def importJsonDictionaryFromFile(cfgf):
+    if file_exists(cfgf):
+        # incarc fisier configurare
+        configFile = open(cfgf, "r")
+        configFileContent = configFile.readlines()
+
+        if len(configFileContent) > 0:
+            jsonConfig = json.loads("".join(configFileContent))
+        else:
+            jsonConfig = {cfgf: {}}
+
+        configFile.close()
+    else:
+        jsonConfig = {cfgf: {}}
+
+    return jsonConfig
 
 
 class iruart(peripheral):
 
-    version = 0.1
-    sendPrefix = b'\xA1\xF1'
+    version = 0.2
 
     def __init__(self, options={
         "id": 2,
@@ -27,13 +70,27 @@ class iruart(peripheral):
         self.pType = "iruart"
         self.pClass = "OUT"
 
+        self.learnThis = False
+
         self.commands["emit"] = sendSignal
+        self.commands["learn"] = learnSignal
 
         self.dictionary = {
-            "POWER": self.sendPrefix+b'\x04\xfb\x08',
-            "CHUP": self.sendPrefix+b'\x04\xfb\x00',
-            "CHDOWN": self.sendPrefix+b'\x04\xfb\x01'
+            # "default": {
+            #     "prefix": 'a1f1',
+            #     "commands": {
+            #         "POWER": '04fb08',
+            #         "CHUP": '04fb00',
+            #         "CHDOWN": '04fb01',
+            #         "MUTE": '04fb09',
+            #     }
+            # }
         }
+
+        for cdfs in filesListAllType():
+            if file_exists(cdfs+".ir"):
+                self.dictionary[cdfs] = importJsonDictionaryFromFile(
+                    cdfs+".ir")
 
         self.__lastRead = False
         self.__tmpValue = False
@@ -70,31 +127,47 @@ class iruart(peripheral):
     def value(self, val):
         self.__lastRead = val
 
-    @peripheral._trigger
-    def send(self, key):
-        if key in self.dictionary.keys():
-            self.uart.write(self.dictionary.get(key))
-            return {"sent": key}
+    def decodedValue(self):
+        toRet = False
 
-        return {"sent": "error"}
-
-    def learn(self, key):
-        pass
-
-    def loadDictionary(self, newDict={}):
-        self.dictionary = newDict
-
-    def getState(self):
         if self.value.__class__.__name__ == 'bytes':
             try:
                 toRet = hexlify(self.value).decode('utf-8')
             except UnicodeError as u:
                 toRet = "Error "+u
 
-            return {"value": toRet}
-            # return {"value": hexlify(self.value).decode('utf-8')}
-        else:
-            return {"value": self.value}
+        return toRet
+
+    @peripheral._trigger
+    def send(self, key, dictionary="default"):
+        if dictionary in self.dictionary.keys():
+            jsonDict = self.dictionary.get(dictionary)
+            if key in jsonDict["commands"].keys():
+                prefix = jsonDict["prefix"]
+                cmd = jsonDict["commands"].get(key)
+                signal = unhexlify(prefix)+unhexlify(cmd)
+
+                self.uart.write(signal)
+
+                return {"sent": {dictionary: key}}
+
+        return {"sent": "error"}
+
+    def sendRaw(self, key):
+        self.uart.write(key)
+
+    @peripheral._trigger
+    def learn(self, key, dictionary):
+        self.learnThis = {dictionary:key}
+
+    def loadDictionary(self, newDict={}):
+        self.dictionary = newDict
+
+    def getState(self):
+        return {
+            "value": self.decodedValue(),
+            "dicts": self.dictionary
+        }
 
     def getObservableMethods(self):
         return ["command", "emit"]
