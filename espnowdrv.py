b2c79290e3f4e4637babd81789ee01c0826f45a3
Cezar Lucan <cezar.lucan@gmail.com>
2022-07-09 14:56:41 +0300

    0.7.0 espnowdrv improved

--- espnowdrv.py
+++ espnowdrv.py
@@ -1,4 +1,3 @@
-import conf
 from peripheral import peripheral
 from time import sleep
 import _thread
@@ -26,11 +25,14 @@ def enable(s):
 def disable(s):
     return s.deinit()
 
+# https://micropython-glenn20.readthedocs.io/en/latest/library/espnow.html#espnow-and-wifi-operation
+
 
 class espnowdrv(peripheral):
     espnow = None
     peers = {}
     stop = False
+    broadcast = 'ffffffffffff'
 
     def __init__(self, options={"autostart": True}):
         super().__init__(options)
@@ -65,11 +67,48 @@ class espnowdrv(peripheral):
             print("THREAD EXCEPTION")
 
     def loadPeers(self):
-        self.peers = importJsonDictionaryFromFile("peers.json")
-        for peer in self.peers:
-            self.espnow.add_peer(ubinascii.unhexlify(self.peers.get(peer)))
+        peers = importJsonDictionaryFromFile("peers.json")
+        for peer in peers:
+            self.loadPeer(peers.get(peer))
 
-        return self.peers
+        return peers
+
+    def loadPeer(self, peer):
+        success = True
+
+        try:
+            self.espnow.add_peer(self.__encodeHexBytes(peer))
+        except:
+            success = False
+
+        return success
+
+    def removePeer(self, peer):
+        success = True
+
+        try:
+            self.espnow.del_peer(self.__encodeHexBytes(peer))
+        except:
+            success = False
+
+        return success
+
+    def peerInfo(self, peer):
+        try:
+            pi = self.espnow.get_peer(self.__encodeHexBytes(peer))
+        except:
+            pi = ()
+
+        if len(pi) == 5:
+            return {
+                "mac": self.__decodeHexBytes(pi[0]),
+                "lmk": pi[1] if pi[1].__class__.__name__ == "str" else pi[1].decode("utf-8"),
+                "channel": pi[2],
+                "ifidx": pi[3],
+                "encrypt": pi[4]
+            }
+        else:
+            return {}
 
     def deinit(self):
         self.espnow.active(False)
@@ -88,11 +127,19 @@ class espnowdrv(peripheral):
         print(source)
         print(message)
 
+    def getPeersByMac(self):
+        peers = []
+        #   0   1       2       3       4
+        # (mac, lmk, channel, ifidx, encrypt (bool))
+        for peer in self.espnow.get_peers():
+            peers.append(self.__decodeHexBytes(peer[0]))
+
+        return peers
+
     def getState(self):
         return {
             "connected": self.isconnected(),
-            "registered_peers": self.espnow.get_peers(),
-            "recorded_peers": self.peers,
+            "peers": self.getPeersByMac(),
             "stats": self.espnow.stats()
         }
 
@@ -111,6 +158,12 @@ class espnowdrv(peripheral):
         else:
             return None
 
+    def __decodeHexBytes(self, val):
+        return ubinascii.hexlify(val).decode("utf-8")
+
+    def __encodeHexBytes(self, val):
+        return ubinascii.unhexlify(bytes(val, "utf-8"))
+
     def run(self):
         a_lock = _thread.allocate_lock()
         while True:
@@ -118,17 +171,19 @@ class espnowdrv(peripheral):
                 host, msg = self.espnow.recv()
                 if msg:
                     rcvdMsg = {}
+
                     try:
-                        rcvdMsg = ujson.loads(msg.decode())
+                        rcvdMsg = ujson.loads(msg.decode("utf-8"))
                     except ValueError:
-                        rcvdMsg = {"message": msg.decode()}
+                        rcvdMsg = {"message": msg.decode("utf-8")}
+                        if rcvdMsg["message"] == "BCAST_REG_PEER":
+                            self.loadPeer(self.__decodeHexBytes(host))
                     except:
                         rcvdMsg = {"error": "other"}
 
-                    rcvdMsg["FROM"] = ubinascii.hexlify(host)
+                    rcvdMsg["FROM"] = self.__decodeHexBytes(host)
 
-                    self.rawMessage(
-                        **{"source": host, "message": msg.decode()})
+                    self.rawMessage(rcvdMsg["FROM"], msg.decode("utf-8"))
 
                     self.message = rcvdMsg
 
@@ -142,10 +197,11 @@ class espnowdrv(peripheral):
         response = False
 
         if to == "*":
-            self.espnow.send(msg, sync)
+            self.espnow.send(msg)
         else:
             if len(to) == 12:
-                response = self.espnow.send(ubinascii.unhexlify(to), msg, sync)
+                encTo = self.__encodeHexBytes(to)
+                response = self.espnow.send(encTo, msg, sync)
 
         return {
             "to": to,
